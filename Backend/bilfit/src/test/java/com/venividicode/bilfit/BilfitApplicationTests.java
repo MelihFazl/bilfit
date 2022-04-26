7aac51ea7fab2866d88098921e5d8897e92a786e
ferhatkorkmaz11 <ferhatkorkmaz@email.com>
2022-04-26 22:45:31 +0300

    initializing spring project

---
+++ Backend/bilfit/src/test/java/com/venividicode/bilfit/BilfitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.venividicode.bilfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilfitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
