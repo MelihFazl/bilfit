27a0f8051273395e72885032f322f9e0e997a520
Kerem ŞAHİN <keremsahin1905@gmail.com>
2022-04-27 23:17:20 +0300

    Addition of dropdown menus for navigation bar.

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,31 @@
+export const MenuItems = [
+    [
+        {
+        title: 'Make Reservation',
+        path: '/make-reservation',
+        cName: 'dropdown-link'
+        },
+        {
+        title: 'My Reservations',
+        path: '/my-reservations',
+        cName: 'dropdown-link'
+        }
+    ],
+    [
+        {
+        title: 'Enroll Tournament',
+        path: '/enroll-tournament',
+        cName: 'dropdown-link'
+        },
+        {
+        title: 'My Tournaments',
+        path: '/my-tournaments',
+        cName: 'dropdown-link'
+        },
+        {
+        title: 'Tournament Requests',
+        path: '/tournament-requests',
+        cName: 'dropdown-link'
+        }
+    ]
+];
